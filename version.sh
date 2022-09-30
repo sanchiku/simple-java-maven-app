2859ef72233ac3a50d9a2b9a96551f05521a6483
Sanchit Kumar <39479874+sanchiku@users.noreply.github.com>
2022-09-30 19:42:25 +0530

    Update version.sh

--- version.sh
+++ version.sh
@@ -3,5 +3,5 @@ VERSION=`cat /var/lib/jenkins/version`
 echo $major_ver
 echo "VERSION:$VERSION"
 CURR_VERSION=$(echo $VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
-echo $CURR_VERSION > version
+echo $CURR_VERSION > /var/lib/jenkins/version
 echo $CURR_VERSION
