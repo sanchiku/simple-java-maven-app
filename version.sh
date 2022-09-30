e65f6287bbeee019db635c5d351b35c1a09a06ab
Sanchit Kumar <39479874+sanchiku@users.noreply.github.com>
2022-09-30 19:20:39 +0530

    Create version.sh

---
+++ version.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+VERSION=`cat version`
+echo "VERSION:$VERSION"
+CURR_VERSION=$(echo $VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
+echo $CURR_VERSION > version
+echo $CURR_VERSION
