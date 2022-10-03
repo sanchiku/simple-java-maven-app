5a06d21114f1b683f56832348e16514d80e735a9
Sanchit Kumar <39479874+sanchiku@users.noreply.github.com>
2022-10-03 11:54:56 +0530

    Update version.sh

--- version.sh
+++ version.sh
@@ -6,7 +6,7 @@ echo $major
 #echo "VERSION:$VERSION"
 if [ $major == $pre_major ]
 then
-        echo "Major versions are equal"
+        #echo "Major versions are equal"
         CURR_VERSION=$(echo $pre_version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
         echo $CURR_VERSION > /var/lib/jenkins/version
         echo $CURR_VERSION
