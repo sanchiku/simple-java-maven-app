eff0c3f2590a0ed24c26a15c19d4a274ba8377d3
Sanchit Kumar <39479874+sanchiku@users.noreply.github.com>
2022-10-03 13:20:41 +0530

    Update version.sh

--- version.sh
+++ version.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 major=$1
-pre_version=`cat /var/lib/jenkins/version`
-pre_major=`cat /var/lib/jenkins/version | awk -F. '{print $1}'`
+file=/var/lib/jenkins/version
+pre_version=`cat $file`
+pre_major=`cat $file | awk -F. '{print $1}'`
 echo $major
 #echo "VERSION:$VERSION"
 if [ $major == $pre_major ]
 then
         #echo "Major versions are equal"
         CURR_VERSION=$(echo $pre_version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
-        echo $CURR_VERSION > /var/lib/jenkins/version
+        echo $CURR_VERSION > $file
         echo $CURR_VERSION
 else
         echo "Major versions are not equal"
-        echo $major.0.0 > /var/lib/jenkins/version
+        echo $major.0.0 > $file
 fi
